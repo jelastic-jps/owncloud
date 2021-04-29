7d755fa1443b2e62f9135cf9561073abddbb53d5
Panslothda <19254254+panslothda@users.noreply.github.com>
2021-04-29 17:55:08 +0200

    Update beforeinstall.js

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -1,3 +1,10 @@
+var resp = {
+  result: 0,
+  ssl: !!jelastic.billing.account.GetQuotas('environment.jelasticssl.enabled').array[0].value,
+  nodes: []
+}
+
+
 if ('${settings.clustered-storage}' == 'true') {
   resp.nodes.push({
     nodeType: "storage",
