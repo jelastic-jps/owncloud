0625da88312660027dc526368fa52245ad8d7b29
Panslothda <19254254+panslothda@users.noreply.github.com>
2021-05-02 00:17:29 +0200

    added storage node mounting

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -5,37 +5,141 @@ var resp = {
   nodes: []
 }
 
-if (${settings.clustered-storage:false}) {   
-  resp.nodes.push({
-    nodeType: "storage",
-    count: 3,
-    cluster: true,
-    flexibleCloudlets: ${settings.st_flexibleCloudlets:8},
-    fixedCloudlets: ${settings.st_fixedCloudlets:1},
-    nodeGroup: "storage",
-    restartDelay: 10,
-    isRedeploySupport: false,
-    validation: {
-      minCount: 3,
-      maxCount: 3
-    }
-  })
+if (${settings.clustered-storage:false}) {
+  if (${settings.litespeed:false}) {
+    resp.nodes.push({
+      nodeType: "litespeedphp",
+      tag: "6.0-php-7.4.15",
+      engine: "7.4",
+      count: 1,
+      flexibleCloudlets: ${settings.cp_flexibleCloudlets:16},
+      fixedCloudlets: ${settings.cp_fixedCloudlets:1},
+      nodeGroup: "cp",
+      env: {
+        SERVER_WEBROOT: "/var/www/webroot/ROOT",
+        REDIS_ENABLED: "true"
+      },
+      volumeMounts: {
+        "/var/www/webroot/ROOT": {
+          readOnly: "false",
+          sourcePath: "/data/ROOT",
+          sourceNodeGroup: "storage"
+          }
+        }  
+      }, {
+      nodeType: "storage",
+      count: 3,
+      cluster: true,
+      flexibleCloudlets: ${settings.st_flexibleCloudlets:8},
+      fixedCloudlets: ${settings.st_fixedCloudlets:1},
+      nodeGroup: "storage",
+      restartDelay: 10,
+      isRedeploySupport: false,
+      validation: {
+        minCount: 3,
+        maxCount: 3
+      }
+    })
+    } else {
+    resp.nodes.push({    
+      nodeType: "nginxphp",
+      count: 1,
+      flexibleCloudlets: ${settings.cp_flexibleCloudlets:16},                  
+      fixedCloudlets: ${settings.cp_fixedCloudlets:1},
+      nodeGroup: "cp",
+      env: {
+        SERVER_WEBROOT: "/var/www/webroot/ROOT",
+        REDIS_ENABLED: "true"
+      },
+      volumeMounts: {
+        "/var/www/webroot/ROOT": {
+          readOnly: "false",
+          sourcePath: "/data/ROOT",
+          sourceNodeGroup: "storage"
+          }
+        }  
+      }, {
+      nodeType: "storage",
+      count: 3,
+      cluster: true,
+      flexibleCloudlets: ${settings.st_flexibleCloudlets:8},
+      fixedCloudlets: ${settings.st_fixedCloudlets:1},
+      nodeGroup: "storage",
+      restartDelay: 10,
+      isRedeploySupport: false,
+      validation: {
+        minCount: 3,
+        maxCount: 3
+      }
+    })
+  }
 } else {
-  if (${settings.ded-storage:false}) {
-  resp.nodes.push({
-    nodeType: "storage",
-    count: 1,
-    flexibleCloudlets: ${settings.st_flexibleCloudlets:8},
-    fixedCloudlets: ${settings.st_fixedCloudlets:1},
-    nodeGroup: "storage",
-    isRedeploySupport: false,
-    validation: {
-      minCount: 1,
-      maxCount: 1
-    }
-  })
-}
-}
+if (${settings.ded-storage:false}) {
+  if (${settings.litespeed:false}) {
+    resp.nodes.push({
+      nodeType: "litespeedphp",
+      tag: "6.0-php-7.4.15",
+      engine: "7.4",
+      count: 1,
+      flexibleCloudlets: ${settings.cp_flexibleCloudlets:16},
+      fixedCloudlets: ${settings.cp_fixedCloudlets:1},
+      nodeGroup: "cp",
+      env: {
+        SERVER_WEBROOT: "/var/www/webroot/ROOT",
+        REDIS_ENABLED: "true"
+      },
+      volumeMounts: {
+        "/var/www/webroot/ROOT": {
+          readOnly: "false",
+          sourcePath: "/data/ROOT",
+          sourceNodeGroup: "storage"
+          }
+        }  
+      }, {
+      nodeType: "storage",
+      count: 1,
+      flexibleCloudlets: ${settings.st_flexibleCloudlets:8},
+      fixedCloudlets: ${settings.st_fixedCloudlets:1},
+      nodeGroup: "storage",
+      isRedeploySupport: false,
+      validation: {
+        minCount: 1,
+        maxCount: 1
+      }
+    })
+  } else {
+    resp.nodes.push({    
+      nodeType: "nginxphp",
+      count: 1,
+      flexibleCloudlets: ${settings.cp_flexibleCloudlets:16},                  
+      fixedCloudlets: ${settings.cp_fixedCloudlets:1},
+      nodeGroup: "cp",
+      env: {
+        SERVER_WEBROOT: "/var/www/webroot/ROOT",
+        REDIS_ENABLED: "true"
+      },
+      volumeMounts: {
+        "/var/www/webroot/ROOT": {
+          readOnly: "false",
+          sourcePath: "/data/ROOT",
+          sourceNodeGroup: "storage"
+          }
+        }  
+      }, {
+      nodeType: "storage",
+      count: 1,
+      flexibleCloudlets: ${settings.st_flexibleCloudlets:8},
+      fixedCloudlets: ${settings.st_fixedCloudlets:1},
+      nodeGroup: "storage",
+      isRedeploySupport: false,
+      validation: {
+        minCount: 1,
+        maxCount: 1
+      }
+    })
+  }
+}}
+
 resp.nodes.push({
   nodeType: "mariadb-dockerized",
   flexibleCloudlets: ${settings.db_flexibleCloudlets:16},
@@ -49,21 +153,25 @@ resp.nodes.push({
     }
 })
 
-if (${settings.litespeed:false}) {
-  resp.nodes.push({
-    nodeType: "litespeedphp:latest",
-    count: 1,
-    flexibleCloudlets: ${settings.cp_flexibleCloudlets:16},
-    fixedCloudlets: ${settings.cp_fixedCloudlets:1},
-    nodeGroup: "cp",
-    env: {
-      SERVER_WEBROOT: "/var/www/webroot/ROOT",
-      REDIS_ENABLED: "true"
+
+if (!${settings.ded-storage:false}) {
+  if (${settings.litespeed:false}) {
+    resp.nodes.push({
+      nodeType: "litespeedphp",
+      tag: "6.0-php-7.4.15",
+      engine: "7.4",
+      count: 1,
+      flexibleCloudlets: ${settings.cp_flexibleCloudlets:16},
+      fixedCloudlets: ${settings.cp_fixedCloudlets:1},
+      nodeGroup: "cp",
+      env: {
+        SERVER_WEBROOT: "/var/www/webroot/ROOT",
+        REDIS_ENABLED: "true"
     }
   })
-} else {
+  } else {
   resp.nodes.push({
-    nodeType: "nginxphp:latest",
+    nodeType: "nginxphp",
     count: 1,
     flexibleCloudlets: ${settings.cp_flexibleCloudlets:16},                  
     fixedCloudlets: ${settings.cp_fixedCloudlets:1},
@@ -73,6 +181,6 @@ if (${settings.litespeed:false}) {
       REDIS_ENABLED: "true"
     }
   })
-}
+}}
 
 return resp;
